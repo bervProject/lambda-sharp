13d9965544603c6051cd99df5e97bf5be5c73896
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-28 21:49:39 +0700

    Update LambdaEntryPoint.cs

--- src/SimpleAPI/LambdaEntryPoint.cs
+++ src/SimpleAPI/LambdaEntryPoint.cs
@@ -19,7 +19,7 @@ public class LambdaEntryPoint :
     // Note: When using the AWS::Serverless::Function resource with an event type of "HttpApi" then payload version 2.0
     // will be the default and you must make Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction the base class.
 
-    Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
+    Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
 {
     /// <summary>
     /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class

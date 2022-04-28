c9e934541ec44c98909d54787c2fa1e36e5cc716
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-28 21:42:08 +0700

    Update LambdaEntryPoint.cs

--- src/SimpleAPI/LambdaEntryPoint.cs
+++ src/SimpleAPI/LambdaEntryPoint.cs
@@ -1,9 +1,9 @@
 namespace SimpleAPI;
 
 /// <summary>
-/// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the 
+/// This class extends from APIGatewayProxyFunction which contains the method FunctionHandlerAsync which is the
 /// actual Lambda function entry point. The Lambda handler field should be set to
-/// 
+///
 /// SimpleAPI::SimpleAPI.LambdaEntryPoint::FunctionHandlerAsync
 /// </summary>
 public class LambdaEntryPoint :
@@ -15,11 +15,11 @@ public class LambdaEntryPoint :
     // API Gateway HTTP API payload version 1.0     -> Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
     // API Gateway HTTP API payload version 2.0     -> Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction
     // Application Load Balancer                    -> Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
-    // 
+    //
     // Note: When using the AWS::Serverless::Function resource with an event type of "HttpApi" then payload version 2.0
     // will be the default and you must make Amazon.Lambda.AspNetCoreServer.APIGatewayHttpApiV2ProxyFunction the base class.
 
-    Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    Amazon.Lambda.AspNetCoreServer.ApplicationLoadBalancerFunction
 {
     /// <summary>
     /// The builder has configuration, logging and Amazon API Gateway already configured. The startup class
@@ -33,8 +33,8 @@ public class LambdaEntryPoint :
     }
 
     /// <summary>
-    /// Use this override to customize the services registered with the IHostBuilder. 
-    /// 
+    /// Use this override to customize the services registered with the IHostBuilder.
+    ///
     /// It is recommended not to call ConfigureWebHostDefaults to configure the IWebHostBuilder inside this method.
     /// Instead customize the IWebHostBuilder in the Init(IWebHostBuilder) overload.
     /// </summary>

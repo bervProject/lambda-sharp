a9083dcdeec75b60b6c98f2facce143d18b422ae
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-04-29 07:49:36 +0700

    format

--- terraform/main.tf
+++ terraform/main.tf
@@ -21,9 +21,9 @@ EOF
 resource "aws_lambda_function" "lambda_container_demo" {
   function_name = "lambda_container_demo"
   role          = aws_iam_role.iam_for_lambda.arn
-  image_uri = "092318301320.dkr.ecr.ap-southeast-1.amazonaws.com/lambda-sharp:main"
-  publish = true
-  package_type = "Image"
+  image_uri     = "092318301320.dkr.ecr.ap-southeast-1.amazonaws.com/lambda-sharp:main"
+  publish       = true
+  package_type  = "Image"
 
   tags = {
     "env" = "dev"
